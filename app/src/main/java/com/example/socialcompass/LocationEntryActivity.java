b0c5839c0c50558dc7aa9090b8dda60c8abf4adb
doublealiu <46761322+doublealiu@users.noreply.github.com>
2023-02-11 20:27:23 -0800

    US-5 finished, persistence and relevant UI elements finished.

--- app/src/main/java/com/example/socialcompass/LocationEntryActivity.java
+++ app/src/main/java/com/example/socialcompass/LocationEntryActivity.java
@@ -2,7 +2,9 @@ package com.example.socialcompass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -42,15 +44,57 @@ public class LocationEntryActivity extends AppCompatActivity {
         labelFriend = findViewById(R.id.friendLabelEditText);
 
         longitudeFamily = findViewById(R.id.familyLongEditText);
-        latitudeFamily = findViewById(R.id.familyLongEditText);
-        labelFamily = findViewById(R.id.familyLongEditText);
+        latitudeFamily = findViewById(R.id.familyLatEditText);
+        labelFamily = findViewById(R.id.familyLabelEditText);
 
         longitudeYou = findViewById(R.id.houseLongEditText);
         latitudeYou = findViewById(R.id.houseLatEditText);
         labelYou = findViewById(R.id.houseLabelEditText);
+
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+
+        longitudeFriend.setText(Util.getFloatAsString(preferences, FRIEND_LONGITUDE));
+        latitudeFriend.setText(Util.getFloatAsString(preferences, FRIEND_LATITUDE));
+        labelFriend.setText(preferences.getString(FRIEND_LABEL, ""));
+
+        longitudeFamily.setText(Util.getFloatAsString(preferences, FAMILY_LONGITUDE));
+        latitudeFamily.setText(Util.getFloatAsString(preferences, FAMILY_LATITUDE));
+        labelFamily.setText(preferences.getString(FAMILY_LABEL, ""));
+
+        longitudeYou.setText(Util.getFloatAsString(preferences, YOU_LONGITUDE));
+        latitudeYou.setText(Util.getFloatAsString(preferences, YOU_LATITUDE));
+        labelYou.setText(preferences.getString(YOU_LABEL, ""));
     }
 
     public void onBackButtonClick(View view) {
-
+        savePreferences();
+        finish(); // back to main activity
     }
+
+    public void onSaveButtonClick(View view) {
+        savePreferences();
+    }
+
+    void savePreferences() {
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        // save relevant fields
+        // family's house
+        Util.saveFloat(editor, longitudeFamily, FAMILY_LONGITUDE);
+        Util.saveFloat(editor, latitudeFamily, FAMILY_LATITUDE);
+        editor.putString(FAMILY_LABEL, labelFamily.getText().toString());
+
+        Util.saveFloat(editor, longitudeFriend, FRIEND_LONGITUDE);
+        Util.saveFloat(editor, latitudeFriend, FRIEND_LATITUDE);
+        editor.putString(FRIEND_LABEL, labelFriend.getText().toString());
+
+        Util.saveFloat(editor, longitudeYou, YOU_LONGITUDE);
+        Util.saveFloat(editor, latitudeYou, YOU_LATITUDE);
+        editor.putString(YOU_LABEL, labelYou.getText().toString());
+
+        editor.apply();
+    }
+
+
 }
--- app/src/main/java/com/example/socialcompass/Util.java
+++ app/src/main/java/com/example/socialcompass/Util.java
@@ -1,9 +1,29 @@
 package com.example.socialcompass;
 
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.widget.EditText;
+
 public final class Util {
     private Util() { }
 
-    public static float parseLongitude(String in) {
-        return 0f;
+    public static void saveFloat(SharedPreferences.Editor editor, EditText uiComponent, String key) {
+        String str = uiComponent.getText().toString();
+        try {
+            if (str != null && !str.equals("")) {
+                float parsed = Float.parseFloat(str);
+                editor.putFloat(key, parsed);
+            }
+        } catch (NumberFormatException e) {
+            Log.e("error", "Could not parse float " + str + ".");
+        }
+    }
+
+    public static String getFloatAsString(SharedPreferences map, String key) {
+        if (map.contains(key)) {
+            return String.valueOf(map.getFloat(key, 0F));
+        } else {
+            return "";
+        }
     }
 }
