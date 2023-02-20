a9b88971d34e4e32257644a14cf59c03560de1b2
Christopher Hughes <clhughes@ucsd.edu>
2023-02-19 23:47:35 -0800

    Added comments for Util

--- app/src/main/java/com/example/socialcompass/Util.java
+++ app/src/main/java/com/example/socialcompass/Util.java
@@ -4,10 +4,16 @@ import android.content.SharedPreferences;
 import android.util.Log;
 import android.widget.EditText;
 
-
+/**
+ * Util class in order to save each float in the activities.
+ * Also parses floats into strings as necessary.
+ */
 public final class Util {
     private Util() { }
 
+    /*
+    Saves ui components
+     */
     public static void saveFloat(SharedPreferences.Editor editor, EditText uiComponent, String key) {
         String str = uiComponent.getText().toString();
         try {
@@ -22,6 +28,9 @@ public final class Util {
         }
     }
 
+    /*
+    Returns string in float format
+     */
     public static String getFloatAsString(SharedPreferences map, String key) {
         if (map.contains(key)) {
             return String.valueOf(map.getFloat(key, 0F));
