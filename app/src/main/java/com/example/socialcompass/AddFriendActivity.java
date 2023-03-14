51fdc5a72338d7480ab78bfde89ba12abfad04cf
artballesteros <58927442+artballesteros@users.noreply.github.com>
2023-03-13 19:08:46 -0700

    Search for friend on server and prompt if success after storing in db

--- app/src/main/java/com/example/socialcompass/AddFriendActivity.java
+++ app/src/main/java/com/example/socialcompass/AddFriendActivity.java
@@ -4,21 +4,34 @@ import static com.example.socialcompass.AddFriendActivity.UI_DEGREES;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.socialcompass.model.LocationDatabase;
+import com.example.socialcompass.model.LocationRepository;
 
 
 public class AddFriendActivity extends AppCompatActivity {
     private EditText degrees;
+    private EditText addFriendEditText;
 
     public static final String UI_DEGREES = "degreeLabel";
+    private LocationRepository repo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_friend);
+
+        // set up db and api
+        var db = LocationDatabase.getSingleton(this);
+        repo = new LocationRepository(db.locationDao());
+
+        addFriendEditText = findViewById(R.id.addFriendEditText);
 
         SharedPreferences preferences = getSharedPreferences("shared", MODE_PRIVATE);
         degrees = findViewById(R.id.degreesEditText);
@@ -31,7 +44,15 @@ public class AddFriendActivity extends AppCompatActivity {
     }
 
     public void onSaveButtonClick(View view) {
-
+        Toast.makeText(getApplicationContext(), "Looking for friend...", Toast.LENGTH_SHORT).show();
+        var publicCode = addFriendEditText.getText().toString();
+        var location = repo.getRemote(publicCode);
+        if (location == null) {
+            Toast.makeText(getApplicationContext(), "Friend doesn't exist!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Toast.makeText(getApplicationContext(), location.label + " added!", Toast.LENGTH_SHORT).show();
+        repo.addLocationToDb(location);
     }
 
     public void onMockDegreesButtonClick(View view) {
